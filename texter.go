631b502b89953c985fec41ddf0a7a588691e342c
Nisarga Patel <np8@umbc.edu>
2018-03-04 00:21:55 -0500

    Added threshold

--- texter.go
+++ texter.go
@@ -9,6 +9,8 @@ import (
 	"os/exec"
 	"strings"
 	"time"
+	"regexp"
+	"strconv"
 )
 
 type Config struct {
@@ -17,15 +19,20 @@ type Config struct {
 	IntervalInSeconds int    `json:"intervalInSeconds"`
 	NumberTo          string `json:"numberTo"`
 	NumberFrom        string `json:"numberFrom"`
+	TopThreshold	  int	 `json:"topThreshold"`
 }
 
-func RunGPUTempCommand() string {
+func RunGPUTempCommand() int {
 	out, err := exec.Command("nvidia-smi", "--query-gpu=temperature.gpu", "--format=csv,noheader").Output()
 	log.Println(string(out[:]))
 	if err != nil {
 		log.Fatal(err)
 	}
-	return string(out[:])
+	toString := string(out[:])
+	reg, _ := regexp.Compile("[^a-zA-Z0-9]")
+	toString = reg.ReplaceAllString(toString, "")
+	i, _ := strconv.Atoi(toString)
+	return i
 }
 
 // This function gets the configuration from the file
@@ -46,7 +53,11 @@ func GetConfigFromFile() Config {
 func SendGPUTempText(config Config) bool {
 	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + config.TwilioAccountSid + "/Messages"
 	gpuTemp := RunGPUTempCommand()
-	messageBody := "Your GPU's current temperature is: " + gpuTemp + " degrees Celsius"
+	log.Println(gpuTemp < config.TopThreshold)
+	if gpuTemp < config.TopThreshold {
+		return false
+	}
+	messageBody := "Your GPU's current temperature is above your set threshold " + strconv.Itoa(config.TopThreshold) + " Celsius and is at " + strconv.Itoa(gpuTemp) + " degrees Celsius"
 	msgData := url.Values{}
 	msgData.Set("To", config.NumberTo)
 	msgData.Set("From", config.NumberFrom)
